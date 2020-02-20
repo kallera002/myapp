2886813fc5e2675e517c3f9c43e54ba320150dbe
Fikri <fikri@inarts.co.id>
2020-02-20 09:46:30 +0700

    Updates

---
+++ precache-manifest.a170af9475a7a2b8bbc756d74d92e3b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50babc1d44266e0e4162413deec7a3da",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "70c540900f34b02ce778",
+    "url": "/myapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bd07b41fd8b200aa0983",
+    "url": "/myapp/static/js/2.ed930f27.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/myapp/static/js/2.ed930f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70c540900f34b02ce778",
+    "url": "/myapp/static/js/main.c995c8b1.chunk.js"
+  },
+  {
+    "revision": "03e71c5bca4215fda27b",
+    "url": "/myapp/static/js/runtime-main.f91f8f69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
